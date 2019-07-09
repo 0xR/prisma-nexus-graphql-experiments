154f8f5f7710233488794ebc4dc905e3fe2b2e69
Ruben Oostinga <ruben.oostinga@vanderlande.com>
2019-07-09 09:39:02 +0100

    Add nexus-prisma

---
+++ prisma-experiment/nexus-server.ts
@@ -0,0 +1,35 @@
+import { prisma } from './generated/prisma-client'
+import datamodelInfo from './generated/nexus-prisma'
+import * as path from 'path'
+import { stringArg, idArg } from 'nexus'
+import { prismaObjectType, makePrismaSchema } from 'nexus-prisma'
+import { GraphQLServer } from 'graphql-yoga'
+
+const Query = prismaObjectType({
+  name: 'Query',
+  definition(t) {
+      t.prismaFields(['*'])
+  },
+})
+
+const Mutation = prismaObjectType({
+  name: 'Mutation',
+  definition(t) {
+    t.prismaFields(['createUser'])
+  },
+})
+
+const schema = makePrismaSchema({
+  types: [Query, Mutation],
+
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/nexus.ts'),
+  },
+})
+
