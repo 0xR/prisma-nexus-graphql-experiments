ec5f212e4a4866eabe5ce59221d1fce7f2478724
Ruben Oostinga <ruben.oostinga@vanderlande.com>
2019-07-09 15:41:18 +0100

    Add a prisma client example

--- client-example.ts
+++ client-example.ts
@@ -1,14 +1,30 @@
-import { prisma } from './generated/prisma-client'
+import { prisma } from "./generated/prisma-client";
 
 // A `main` function so that we can use async/await
-async function main() {
+async function create() {
   // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice', password: 'qwerty' })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+  const newUser = await prisma.createUser({
+    name: "Alice",
+    password: "qwerty"
+  });
+  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
 
   // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+  const allUsers = await prisma.users();
+  console.log(allUsers);
 }
 
-main().catch(e => console.error(e))
+// create().catch(e => console.error(e));
+
+async function subscribe() {
+  // Create a new user called `Alice`
+  const updatedUser = await prisma.$subscribe.user();
+  for await (const user of {
+    [Symbol.asyncIterator]() {
+      return updatedUser;
+    }
+  }) {
+    console.log(`Update user ${JSON.stringify(user, null, 2)}`);
+  }
+}
+subscribe().catch(e => console.error(e));
