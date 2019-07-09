27aca74b7ec5ab6b4cd2c764795d4a5b2c773cf4
Ruben Oostinga <ruben.oostinga@vanderlande.com>
2019-07-09 12:14:27 +0100

    Add some nexus examples

--- nexus-server.ts
+++ nexus-server.ts
@@ -1,35 +1,61 @@
-import { prisma } from './generated/prisma-client'
-import datamodelInfo from './generated/nexus-prisma'
-import * as path from 'path'
-import { stringArg, idArg } from 'nexus'
-import { prismaObjectType, makePrismaSchema } from 'nexus-prisma'
-import { GraphQLServer } from 'graphql-yoga'
+import { prisma } from "./generated/prisma-client";
+import datamodelInfo from "./generated/nexus-prisma";
+import * as path from "path";
+import { prismaObjectType, makePrismaSchema } from "nexus-prisma";
+import { GraphQLServer } from "graphql-yoga";
 
 const Query = prismaObjectType({
-  name: 'Query',
+  name: "Query",
   definition(t) {
-      t.prismaFields(['*'])
-  },
-})
+    t.prismaFields(["*"]);
+  }
+});
 
 const Mutation = prismaObjectType({
-  name: 'Mutation',
+  name: "Mutation",
   definition(t) {
-    t.prismaFields(['createUser'])
-  },
-})
+    t.field("createUser", {
+      ...t.prismaType.createUser,
+      args: {
+        ...t.prismaType.createUser.args
+      },
+      resolve(root, args, ctx) {
+        if (args.data.password !== "asdf") {
+          throw new Error("invalid password");
+        }
+        return t.prismaType.createUser.resolve(root, args, ctx);
+      }
+    });
+  }
+});
+
+const User = prismaObjectType({
+  name: "User",
+  definition(t) {
+    t.prismaFields(["*"]);
+    // t.prismaFields({ filter: ['password']});
+    t.string("uppercaseName", {
+      resolve: user => user.name.toUpperCase()
+    });
+  }
+});
 
 const schema = makePrismaSchema({
-  types: [Query, Mutation],
+  types: [Query, Mutation, User],
 
   prisma: {
     datamodelInfo,
-    client: prisma,
+    client: prisma
   },
 
   outputs: {
-    schema: path.join(__dirname, './generated/schema.graphql'),
-    typegen: path.join(__dirname, './generated/nexus.ts'),
-  },
-})
+    schema: path.join(__dirname, "./generated/schema.graphql"),
+    typegen: path.join(__dirname, "./generated/nexus.ts")
+  }
+});
 
+const server = new GraphQLServer({
+  schema,
+  context: { prisma }
+});
+server.start(() => console.log("Server is running on http://localhost:4000"));
